5a4142736e856dec1c681557388279ddc5825cb7
thisisana <ana.gabriela.mestre@est.fib.upc.edu>
2017-11-14 11:11:29 +0100

    Packing done

---
+++ Other problems/Packing/Makefile
@@ -0,0 +1,6 @@
+file = packing
+
+$(file): $(file).pl
+	swipl -quiet -O -g main --stand_alone=true -o $(file) -c $(file).pl
+
+
---
+++ Other problems/Packing/entradaPacking1.pl
@@ -0,0 +1,16 @@
+% Width and height of the large piece of cloth
+width(3).
+height(5).
+
+% rect(pieceNum,pieceWidth,pieceHeight)
+rect(1,2,2).
+rect(2,2,2).
+rect(3,1,4).
+rect(4,3,1).
+
+%% Possible solution:
+%% 3  1  1 
+%% 3  1  1 
+%% 3  2  2 
+%% 3  2  2 
+%% 4  4  4 
