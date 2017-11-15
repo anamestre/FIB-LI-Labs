377a13d5d43c4381c6e715ff7c24c6e86c064d91
thisisana <ana.gabriela.mestre@est.fib.upc.edu>
2017-11-15 09:27:31 +0100

    Update repo

--- Prolog problems/otherProblems.pl
+++ Prolog problems/otherProblems.pl
@@ -22,10 +22,10 @@
                                         domino(NewDos, P2),
                                         putDo(Do, P2, P).
                                         
-putDo([X, Y], [], [[X, Y]]).
-putDo([X, Y], [], [[Y, X]]).
-putDo([X, Y], [[Y, Z]|List], [[X, Y], [Y, Z] | List]).
-putDo([Y, X], [[Y, Z]|List], [[X, Y], [Y, Z] | List]).
+    putDo([X, Y], [], [[X, Y]]).
+    putDo([X, Y], [], [[Y, X]]).
+    putDo([X, Y], [[Y, Z]|List], [[X, Y], [Y, Z] | List]).
+    putDo([Y, X], [[Y, Z]|List], [[X, Y], [Y, Z] | List]).
 
 /*
  2) Dada una fórmula proposicional, donde las variables son enteros desde
@@ -75,22 +75,16 @@ putDo([Y, X], [[Y, Z]|List], [[X, Y], [Y, Z] | List]).
         F = -(1 * -2) + 3 * (2+1),  
         assertz(nextvarNum(4)), % necesario para utilizar newvar
         tseitin(F,RootVar), writec([RootVar]), nl, !.
-   
-    tseitin(F*G,X):- newvar(A), tseitin(-A + F, ALGO),
-                                                tseitin(-A + G, ALGO2),
-                                                append(ALGO, ALGO2, X).
     
-    tseitin(F+G,X):- newvar(A), tseitin(A + -F, ALGO),
-                                                tseitin(A + -G, ALGO2),
-                                                append(ALGO, ALGO2, X).
-    
-    
+    tseitin(F*G,X):- newvar(X), tseitin(F, FT), tseitin(G, GT), writeClauses(X=FT*GT), !.
+    tseitin(F+G,X):- newvar(X), tseitin(F, FT), tseitin(G, GT), writeClauses(X=FT+GT), !.
     tseitin(-F,-X):- tseitin(F,X), !.
     tseitin(P,P).
     
-    %writeClauses( Z=X*Y ):- writec([-Z,X]), ..., !.
-   % writeClauses( Z=X+Y ):- ..., !.
+    writeClauses( Z=X*Y ):- writec([-Z,X]), writec([-Z,Y]), writec([Z,-X, -Y]), !.
+    writeClauses( Z=X+Y ):- writec([Z,-X]), writec([Z,-Y]), writec([-Z,X, Y]), !.
     
     writec( []    ):- write('0'), nl,!.
     writec( [X|C] ):- Lit is X, write(Lit), write(' '), writec(C),!.
     newvar(X):- retract(nextvarNum(X)), X1 is X+1, assertz(nextvarNum(X1)),!.
+   
